01147c515cdeeed3e14fb83b45d2d605894f64a0
37trillion <96641975+37trillion@users.noreply.github.com>
2023-05-10 23:17:38 +0000

    Update CryptoCom.py

--- CryptoCom.py
+++ CryptoCom.py
@@ -1,11 +1,15 @@
+import json
+import logging
 import hmac
 from urllib.parse import urljoin
 import hashlib
 import time
+import websocket
+import requests
 
 class CryptoComAPI:
-    
-    def __init__(self, api_key, api_secret, api_url='https://api.crypto.com/v2/'):
+
+    def __init__(self, api_key, api_secret, api_url):
         self.api_key = api_key
         self.api_secret = api_secret
         self.api_url = api_url
@@ -14,12 +18,10 @@ class CryptoComAPI:
         params_str = ''
         for key in sorted(params.keys()):
             params_str += str(key) + str(params[key])
-        signature = hmac.new(bytes(self.api_secret, 'utf-8'), 
+        signature = hmac.new(bytes(self.api_secret, 'utf-8'),
                              bytes(params_str, 'utf-8'), hashlib.sha256).hexdigest()
-        logging.INFO(f"the {_generate_signature} status is {message}")
-        logging._Level(f"{INFO}")
         return signature
-    
+
     def _request(self, method, path, **kwargs):
         url = urljoin(self.api_url, path)
         params = kwargs.get('params', {})
@@ -31,18 +33,18 @@ class CryptoComAPI:
             'X-MBX-APIKEY': self.api_key,
         }
         kwargs['params']['signature'] = self._generate_signature(params)
-        response = requests.request(method, url, **kwargs)
+        response = requests.get(method, url, **kwargs)
         response.raise_for_status()
         return response.json()
 
     def connect(self):
         # Test connection
         try:
-            self._request('GET', 'public/get-instruments')
+            self._request('GET', 'public/auth')
         except Exception as e:
-            logging.error(f"Connection failed: {e}")
+            logging.error(f'Connection failed: {e}')
             return False
-        logging.info("Connection successful.")
+        logging.info('Connection successful.')
         return True
 
     def get_instruments(self):
@@ -51,11 +53,10 @@ class CryptoComAPI:
 
     def get_open_orders(self, symbol):
         params = {'instrument_name': symbol}
-        
+
         response = self._request('GET', 'private/get-open-orders', params=params)
-        logging.INFO(f"The {get_open_orders} status is {message}")
         return response['result']['orders']
-    
+
     def place_order(self, symbol, side, price, quantity):
         params = {
             'instrument_name': symbol,
@@ -65,11 +66,10 @@ class CryptoComAPI:
             'type': 'LIMIT',
             'time_in_force': 'GOOD_TILL_CANCEL',
         }
-        
+
         response = self._request('POST', 'private/create-order', json=params)
-        logging.INFO(f"the {place_order} status is {message}")
         return response['result']['order_id']
-    
+
     def cancel_order(self, symbol, order_id):
         params = {
             'instrument_name': symbol,
@@ -77,21 +77,50 @@ class CryptoComAPI:
         }
         response = self._request('DELETE', 'private/cancel-order', json=params)
         return response['result']['order_id']
-    def parse_ws_message(self, message):
+
+    def on_message(self, ws, message):
         """
-        Parses WebSocket message received from Crypto.com exchange.
+        This function will be called each time a new message is received from the WebSocket connection.
         """
-        message = json.loads(message)
-        if 'e' in message:
-            # This is a response to a subscription request.
-            if message['e'] == 'subscribe':
-                if message['success']:
-                    logging.info(f"Subscription to {message['stream']} successful.")
-                else:
-                    logging.error(f"Subscription to {message['stream']} failed: {message['message']}")
-            # This is a regular data update message.
-            elif message['e'] == 'update':
-                # Do something with the data.
-                pass
-        else:
-            logging.warning(f"Unrecognized WebSocket message: {message}")
+        logging.info(f'Received message: {message}')
+
+        # Send heartbeat message back to the server
+        message_data = json.loads(message)
+        if message_data.get('ping'):
+            ws.send(json.dumps({'pong': message_data['ping']}))
+
+    
+    def start_websocket(self, symbol):
+        endpoint = f"wss://ws.crypto.com/v2/market"
+        ws = websocket.WebSocketApp(endpoint,
+                                    on_open=lambda ws: self._on_open(ws, symbol),
+                                    on_message=lambda ws, message: self._on_message(ws, message),
+                                    on_error=lambda ws, error: self._on_error(ws, error),
+                                    on_close=lambda ws: self._on_close(ws))
+        ws.run_forever()
+
+    def _on_open(self, ws, symbol):
+        logging.info(f"WebSocket connection opened to {ws.url}")
+        self._subscribe(ws, symbol)
+
+    def _on_message(self, ws, message):
+        logging.info(f"Received message: {message}")
+        self.parse_ws_message(message)
+
+    def _on_error(self, ws, error):
+        logging.error(f"WebSocket error: {error}")
+
+    def _on_close(self, ws):
+        logging.info(f"WebSocket connection closed")
+
+    def _subscribe(self, ws, symbol):
+        params = {
+            "method": "subscribe",
+            "id": "1",
+            "params": {
+                "channels": [f"trades.{symbol}"]
+            }
+        }
+        message = json.dumps(params)
+        ws.send(message)
+        logging.info(f"Subscribing to trades for {symbol}")
